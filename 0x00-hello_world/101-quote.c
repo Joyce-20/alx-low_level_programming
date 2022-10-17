91d6cd470eaa63dcbff2a44d11480ecd63991cb5
Joyce-20 <nanatamsil@gmail.com>
2022-10-17 11:05:33 -0700

    last task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -write a c program that prints to the standard error
+ *
+ * Return: 1
+*/
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 61);
+	return (1);
+}
