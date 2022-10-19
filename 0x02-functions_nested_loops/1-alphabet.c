85679ef87be292d98e2b73ea2e310615df65410e
Joyce-20 <nanatamsil@gmail.com>
2022-10-18 17:08:22 -0700

    loops tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in small letter
+ *Return:0
+*/
+
+void print_alphabet(void)
+{
+int k;
+
+for (k = 'a'; k <= 'z'; k++)
+{
+_putchar(k);
+}
+_putchar('\n');
+}
+
